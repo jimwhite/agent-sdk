79e2f1590ec9c30bb381462bfec235c91408d821
Xingyao Wang <xingyao@all-hands.dev>
2025-10-08 16:53:15 +0000

    remove unnecessary stuff

--- openhands/workspace/utils/local.py
+++
@@ -1,9 +0,0 @@
-"""Backwards compatibility re-export of LocalWorkspace.
-
-DEPRECATED: Import from openhands.sdk.workspace.local instead.
-"""
-
-from openhands.sdk.workspace.local import LocalWorkspace
-
-
-__all__ = ["LocalWorkspace"]
--- openhands/workspace/utils/models.py
+++
@@ -1,9 +0,0 @@
-"""Backwards compatibility re-export of workspace models.
-
-DEPRECATED: Import from openhands.sdk.workspace.models instead.
-"""
-
-from openhands.sdk.workspace.models import CommandResult, FileOperationResult
-
-
-__all__ = ["CommandResult", "FileOperationResult"]
